b425ea0f7ec03fac7792480ea698ed2bec26e61a
Хасан Афендин <KhZAfendin@stud.kpfu.ru>
2025-06-13 13:17:40 +0300

    добавил передачу хода при взятии или разыгрывании карты x3

--- src/main/java/org/example/project101game/GameClient.java
+++ src/main/java/org/example/project101game/GameClient.java
@@ -125,7 +125,10 @@ public class GameClient {
                     } else if (msg.startsWith("turn:")) {
                         // Формат: turn:PLAYER_ID
                         currentTurnId = msg.substring(5);
-                        gameController.setIsMyTurn(currentTurnId.equals(myClientId));
+                        if (gameController != null) {
+                            gameController.setIsMyTurn(currentTurnId.equals(myClientId));
+                        } else {
+                        }
                     } else if ("START_GAME".equals(msg)) {
                         // Всё готово — переключаем сцену и передаём данные
                         if (waitingRoomController != null) {
